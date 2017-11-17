b8db4a6aa109bed920e89609946de1fd8a596806
Demis Bellot <demis.bellot@gmail.com>
2017-11-17 05:08:46 -0500

    Add Templates Boostrap Project

---
+++ MyApp/AppHost.cs
@@ -0,0 +1,32 @@
+﻿using Funq;
+using ServiceStack;
+using MyApp.ServiceInterface;
+
+namespace MyApp
+{
+    public class AppHost : AppHostBase
+    {
+        /// <summary>
+        /// Base constructor requires a Name and Assembly where web service implementation is located
+        /// </summary>
+        public AppHost()
+            : base("MyApp", typeof(MyServices).Assembly) { }
+
+        /// <summary>
+        /// Application specific configuration
+        /// This method should initialize any IoC resources utilized by your web service classes.
+        /// </summary>
+        public override void Configure(Container container)
+        {
+            SetConfig(new HostConfig
+            {
+                DebugMode = AppSettings.Get("DebugMode", false),
+                WebHostPhysicalPath = MapProjectPath("~/wwwroot"),
+                AddRedirectParamsToQueryString = true,
+                UseCamelCase = true,
+            });
+
+            Plugins.Add(new TemplatePagesFeature());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# templates-netfx
+
+ServiceStack ASP.NET Templates Bootstrap Project
